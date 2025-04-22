c74b92e3bf05a5d2bd9d8e617a15283aef0f13c6
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-04-22 23:41:13 +0700

    Add button to Home component for user interaction

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,6 +10,7 @@ const Home = () => {
       <Hero />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1>TEST H! NIH</h1>
+        <Button>Wahyu Dontol</Button>
 
         <TeamSection />
       </div>
