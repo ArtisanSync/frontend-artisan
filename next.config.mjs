91f3338dace5060f802dfb5613bb02f717c692db
Wahyu Pinanda Ginting <Keiwahyu19@gmail.com>
2025-05-16 04:05:22 +0800

    fix (service): cicd

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,19 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/images/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
